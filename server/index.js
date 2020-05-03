b83ab7e3fd9179feab4dcc383b05adff96f047b7
Tyler <tylersayvetz@gmail.com>
2020-05-03 12:11:13 -0700

    add connect/disconnect awareness

--- server/index.js
+++ server/index.js
@@ -4,15 +4,30 @@ const http = require('http')
 const server = http.Server(app)
 const io = require('socket.io')(server)
 
+const data = {
+  numUsers: 0,
+}
+
+
 io.on('connection', socket => {
   
   broadcastNewChatter(socket);
   introUser(socket);
 
-  console.log('a user connected')
+  data.numUsers++
+  broadcastNumUsers();
+
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user DISconnected');
+    --data.numUsers
+    broadcastNumUsers();
+  })
+
   socket.on('chat-message', data => {
     console.log('a user sent a chat message:', data)
-    console.log(typeof data);
+
     io.emit('chat-message', data)
   })
   socket.on('typing', (data) => {
@@ -21,6 +36,10 @@ io.on('connection', socket => {
   })  
 
 })
+
+function broadcastNumUsers() {
+  io.emit('update-num-users', data.numUsers)
+}
 
 function introUser(socket) {
   socket.emit('get-name', 'Introduce yourself!')
